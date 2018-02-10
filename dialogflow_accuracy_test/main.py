325d12753a46364d3782eae044b62a7d37c36ad5
= <webf0x@hotmail.com>
2018-02-09 21:34:44 -0500

    Connect to chatbot

--- dialogflow_accuracy_test/main.py
+++ dialogflow_accuracy_test/main.py
@@ -1,5 +1,49 @@
-def main():
-    print("Hello world")
+import apiai
+import argparse
+import json
+import uuid
 
-if __name__ == "__main__":
+
+def main():
+    dialogflow_client_access_token = get_dialogflow_client_access_token()
+    session_id = str(uuid.uuid4())
+
+    chatbot = apiai.ApiAI(dialogflow_client_access_token)
+
+    user_says = 'Hello'
+    json_response = get_json_response(chatbot, session_id, user_says)
+    intent = json_response_to_intent(json_response)
+    speech = json_response_to_speech(json_response)
+    answer_summary = 'user says: "{}" => intent: "{}", speech: "{}"'.format(user_says, intent, speech)
+    print(answer_summary)
+
+
+def json_response_to_intent(json_response):
+    return json_response['result']['metadata']['intentName']
+
+
+def json_response_to_speech(json_response):
+    return json_response['result']['fulfillment']['speech']
+
+
+def get_json_response(chatbot, session_id, user_says):
+    request = chatbot.text_request()
+    request.session_id = session_id
+    request.query = user_says
+    http_response = request.getresponse()
+    response_content = http_response.read()
+    decoded_response = response_content.decode()
+    json_response = json.loads(decoded_response)
+    return json_response
+
+
+def get_dialogflow_client_access_token():
+    parser = argparse.ArgumentParser(description='Process program arguments')
+    parser.add_argument('--dialogflow_client_access_token', type=str,
+                        help='DialogFlow client access token')
+    args = parser.parse_args()
+    return args.dialogflow_client_access_token
+
+
+if __name__ == '__main__':
     main()
--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+#Build and run
+#Parameters:
+#   - $1: dialogflow_client_access_token E.g.: 12312312312mytoken123123123
+
 set -e #Exit on error
 
 tox
-source .tox/py37/Scripts/activate
-python dialogflow_accuracy_test/main.py
+source .tox/py35/Scripts/activate
+python dialogflow_accuracy_test/main.py --dialogflow_client_access_token $1
 
