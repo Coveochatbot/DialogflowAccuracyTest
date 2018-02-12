7991d4698cce1c9f6c97264a926b158391dd69b6
= <webf0x@hotmail.com>
2018-02-11 19:40:42 -0500

    Create intents

--- dialogflow_accuracy_test/main.py
+++ dialogflow_accuracy_test/main.py
@@ -15,6 +15,77 @@ def main():
     delete_all_intents(parent)
     assert(0 == get_number_of_intents(parent))
 
+    intents_to_create = [
+        {
+            'display_name': 'best_university',
+            'training_phrases_parts': [
+                'what is the best university?',
+                'what is the greatest university?',
+                'where should I study?',
+                'can you recommend me an university?'
+            ],
+            'message_texts': [
+                'Sherby',
+                'Sherbrooke',
+                'Université de Sherbrooke',
+                'UdeS'
+            ]
+        },
+        {
+            'display_name': 'best_poutine',
+            'training_phrases_parts': [
+                'who makes the greatest poutine?',
+                'who makes the best poutine?',
+                'where is the greatest poutine?',
+                'where is the best poutine?'
+            ],
+            'message_texts': [
+                'Snack bar Cameleon',
+                'The Snack',
+                'The Cameleon',
+                'That poutine place on Galt street'
+            ]
+        }
+    ]
+    create_intents_from_intent_dicts(parent, intents_to_create)
+    assert(2 == get_number_of_intents(parent))
+
+
+def create_intent(parent, display_name, training_phrases_parts,
+                  message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=message_texts)
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    intents_client.create_intent(parent, intent)
+
+
+def create_intents_from_intent_dicts(parent, intent_dicts):
+    for intent_dict in intent_dicts:
+        create_intent_from_intent_dict(parent, intent_dict)
+
+
+def create_intent_from_intent_dict(parent, intent_dict):
+    create_intent(
+        parent,
+        intent_dict['display_name'],
+        intent_dict['training_phrases_parts'],
+        intent_dict['message_texts']
+    )
+
 
 def get_number_of_intents(parent):
     return len(list(get_all_intents(parent)))
